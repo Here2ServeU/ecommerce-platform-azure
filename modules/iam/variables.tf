29a0e82a6adf02a8d94e8a2160d4f3a238b9aa74
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-23 08:23:09 -0600

    Update variables.tf

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -12,3 +12,18 @@ variable "scope" {
   description = "The scope at which the role assignment applies (e.g., subscription, resource group, or resource)"
   type        = string
 }
+
+variable "iam_principal_id" {
+  description = "The ID of the principal (user, group, or service principal) to assign the role to"
+  type        = string
+}
+
+variable "iam_role_definition_name" {
+  description = "The name of the Azure role to assign (e.g., Contributor, Reader)"
+  type        = string
+}
+
+variable "iam_scope" {
+  description = "The scope at which the role assignment applies (e.g., subscription, resource group, or resource)"
+  type        = string
+}
