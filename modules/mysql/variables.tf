4b2d41236ee4e7ff885e664d62aa6cfd9b2da850
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-23 08:09:03 -0600

    Create variables.tf

---
+++ modules/mysql/variables.tf
@@ -0,0 +1,30 @@
+variable "server_name" {
+  description = "MySQL server name"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password"
+  type        = string
+  sensitive   = true
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for the MySQL server"
+  type        = map(string)
+}
