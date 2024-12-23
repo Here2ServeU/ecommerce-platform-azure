6a68f3de404b97b481b38554247889443bffc2c1
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-23 08:23:54 -0600

    Update variables.tf

--- modules/servicebustopic/variables.tf
+++ modules/servicebustopic/variables.tf
@@ -27,3 +27,33 @@ variable "tags" {
   description = "Tags for the Service Bus"
   type        = map(string)
 }
+
+variable "notification_namespace_name" {
+  description = "The name of the Service Bus namespace for notifications"
+  type        = string
+}
+
+variable "notification_topic_name" {
+  description = "The name of the Service Bus topic for notifications"
+  type        = string
+}
+
+variable "notification_subscription_name" {
+  description = "The name of the Service Bus subscription for notifications"
+  type        = string
+}
+
+variable "notification_resource_group_name" {
+  description = "The name of the resource group for the Service Bus"
+  type        = string
+}
+
+variable "notification_location" {
+  description = "The Azure region where the Service Bus is deployed"
+  type        = string
+}
+
+variable "notification_tags" {
+  description = "Tags to associate with the Service Bus resources"
+  type        = map(string)
+}
